2a928f070b4ac59298b96237a5725fa7bec8aeaa
hbuyocto <hbuyocto@hbuyocto.(none)>
2015-03-20 01:41:18 -0700

    add rtl8192cu driver,folder rtl8192cu_rpi

--- make_ben.sh
+++ make_ben.sh
@@ -8,11 +8,13 @@ export CROSS_COMPILE=arm-poky-linux-gnueabi-
 #make imx_v7_defconfig
 make -j4 uImage LOADADDR=0x10008000
 make imx6q-sabresd.dtb
+make imx6dl-sabresd.dtb
 #make modules
 
 rm uImage
-rm imx6q-sabresd.dtb
+rm *.dtb
 cp arch/arm/boot/uImage ./
 cp arch/arm/boot/dts/imx6q-sabresd.dtb ./
+cp arch/arm/boot/dts/imx6dl-sabresd.dtb ./
 
 echo "build end"
